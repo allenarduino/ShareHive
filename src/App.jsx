5e37f9ad1bc3556be2466951549123778061f014
allenarduino <aljay3334@gmail.com>
2023-05-30 00:30:23 +0000

    Added the global state business logic with useReducer and context api

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return <Text>My App</Text>;
+};
+
+export default App;
---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,17 @@
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        isLoggedIn: true,
+      };
+
+    case 'LOGOUT':
+      return {
+        ...state,
+        isLoggedIn: false,
+      };
+    default:
+      return state;
+  }
+};
